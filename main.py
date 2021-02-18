4530dc0912e15885a40c323dc2a2a5c401084fac
Leonid Imeev <leonidimeev@yandex.ru>
2021-02-18 16:01:50 +0900

    asdasd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import classes
 
-test_player = classes.Player('tuko','Sheriff')
+test_player = classes.Player(classes.PersonCard('Tuko','asddes','asdnote',4,'asdability'),'Sheriff')
+print(test_player.health)
